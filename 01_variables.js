efe4e058bded731b4e5009f11fd3a10593377c5e
zarif007008 <zarif.khan@northsouth.edu>
2024-08-07 10:20:30 +0600

    basic

---
+++ 01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 26
+let accountEmail = "zarif@gmail.com"
+var accountPassword = "123"
+accountCity = "Dhaka"
+let accountState;
+//accountId = 2 // not allowed
+
+accountEmail = "khan@gmail.com"
+accountPassword = "1234"
+accountCity = "rajshahi"
+
+console.log(accountId);
+
+/*
+dont use "var"
+because of issue in block scope and functional scope
+*/
+
+sconsole.table([accountId,accountEmail,accountPassword,accountCity,accountState])
