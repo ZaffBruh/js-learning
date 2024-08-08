d3ce8c719c793cec2c4add60b6d6ca2b9771b015
zarif007008 <zarif.khan@northsouth.edu>
2024-08-09 04:50:22 +0600

    operation

--- conversionOperation.js
+++ conversionOperation.js
@@ -1,4 +1,4 @@
-let score = "33abc"
+/*let score = "33abc"
 console.log(typeof score);
 
 let valueNumber = Number(score);
@@ -29,3 +29,38 @@ let loggedIn = true
 let valueNumber4 = Boolean(loggedIn)
 console.log(loggedIn);
 console.log(valueNumber4);
+
+//"" => false
+//"zarif" => true
+//1 => true
+//0 => false
+
+// ****** Operations **********
+
+*/
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2**3);
+//console.log(typeof negValue);
+
+// 2**3 => 2^3
+
+//console.log(2*3);
+
+let str1 = "hello"
+let str2 = " zarif"
+let str3 = "khan"
+
+/*console.log(str1 + str2+ str3);
+console.log("1"+"2");
+console.log("2"+" 3");
+console.log(1+"a");
+console.log(1+1+"b"+2);
+console.log("c"+1+1);
+*/
+
+console.log(+true);
+console.log(-true);
+console.log(+false);
