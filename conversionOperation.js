8d157b52330f61057cfdcde4c305c92c2ed8f7ca
zarif007008 <zarif.khan@northsouth.edu>
2024-08-08 17:09:23 +0600

    conversion operation

---
+++ conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "33abc"
+console.log(typeof score);
+
+let valueNumber = Number(score);
+console.log(typeof valueNumber);
+console.log(valueNumber);
+
+//NaN = not a number
+
+let score1 = null
+console.log(score1);
+
+let valueNumber1 = Number(score1)
+console.log(typeof valueNumber1);
+console.log(valueNumber1);
+
+let gay = true
+console.log(typeof gay);
+
+let valueNumber2 = Number(gay)
+console.log(valueNumber2);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1;
+//null => 0
+
+let loggedIn = true
+let valueNumber4 = Boolean(loggedIn)
+console.log(loggedIn);
+console.log(valueNumber4);
