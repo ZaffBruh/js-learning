60d9ac4802f1502245c275c5257fd17357d019d9
zarif007008 <zarif.khan@northsouth.edu>
2024-08-12 16:39:25 +0600

    array

---
+++ array.js
@@ -0,0 +1,16 @@
+// array
+
+const myArray = [0, 1, 2, 3, 4, 5, true, "zarif"]
+const myHero = ["spiderman", "batman"]
+
+console.log(myArray[0]);
+console.log(myHero.length);
+console.log(myArray[1]);
+
+//array methods
+
+myArray.push(6)
+myArray.push(7)
+myArray.pop()
+myArray.unshift(9)
+console.log(myArray);
