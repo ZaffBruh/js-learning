201bdd0d534184e4b2bc5a390febb39c86d67935
zarif007008 <zarif.khan@northsouth.edu>
2024-08-13 11:11:48 +0600

    func

--- functions.js
+++ functions.js
@@ -1,2 +1,15 @@
-const test = new Object()
-test.id = 20
+console.log("H");
+console.log("E");
+console.log("L");
+console.log("L");
+console.log("O");
+
+function hello(){
+    console.log("H");
+    console.log("E");
+    console.log("L");
+    console.log("L");
+    console.log("O");
+}
+
+console.log(hello())
