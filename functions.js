66522528af7d71b2174ee1dcaffec0c614024008
zarif007008 <zarif.khan@northsouth.edu>
2024-08-13 11:42:38 +0600

    functions

--- functions.js
+++ functions.js
@@ -12,4 +12,31 @@ function hello(){
     console.log("O");
 }
 
-console.log(hello())
+//console.log(hello())
+
+// function of adding two numbers
+// function addTwoNumbers(number1, number2) {  //parameters
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+    console.log("ZARIF"); //after return, no code will be executed
+}
+
+addTwoNumbers(3 ,5)  //arguments
+const result = addTwoNumbers(3,5)  //if u return in a function, then the value can be passed into a variable
+console.log(`result: ${result}`);
+console.log("result: ",result);
+
+function loginUserMessage(username = "zaff"){
+    if(!username){
+        console.log("please insert a username");
+        return
+    }
+    return `${username} just logged in`
+}
+console.log(loginUserMessage("Zarif"))
+console.log(loginUserMessage())
