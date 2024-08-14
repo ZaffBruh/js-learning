06099353ea2a3802d7ef5142951f5ed45dbd2b08
zarif007008 <zarif.khan@northsouth.edu>
2024-08-14 13:45:36 +0600

    scope

--- scope.js
+++ scope.js
@@ -6,8 +6,41 @@ if(true){
     console.log("inner: ", a);
 }
 
-
 console.log(a);
 // console.log(b);
 //onsole.log(c);
 
+function one(){
+    const username = "zarif"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    //console.log(website);
+    //two()
+}
+//one()
+
+if(true){
+    const username = "zarif"
+    if(username === "zarif"){
+        const website = " youtube"
+        console.log(username + website);  //child function can take the parent function's variable so it will work
+    }
+    //console.log(website);   //parent function can NOT take child function's variable so it will NOT work
+}
+//console.log(username);   //its scope. the username variable can not be accessed outside the if function.
+
+
+//+++++++++++++++++++++++++++++++++++++++
+
+console.log(addOne(5))
+function addOne(num){
+    return num + 1
+}
+
+const addTwo = function(num){
+    return num + 2
+}
+console.log(addTwo(5));
