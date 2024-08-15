0a4ccffd9018a114fc473ace922aaef4fd5fea84
zarif007008 <zarif.khan@northsouth.edu>
2024-08-15 06:47:53 +0600

    execution and call stack

---
+++ execution.js
@@ -0,0 +1,31 @@
+// javascript execution context
+
+//there are three types execution context: 1) global EC 2) function execution context 3) eval EC
+
+let val1 = 10
+let val2 = 5
+function addNum(num1, num2){
+    let total = num1 + num2
+    return total
+}
+//console.log(addNum(val1, val2));
+let result1 = addNum(val1, val2)
+let result2 = addNum(10, 2)
+
+//call stack
+
+function one(){
+    console.log("one");
+    two()
+}
+function two(){
+    console.log("two");
+    three()
+}
+function three(){
+    console.log("three");
+}
+
+one()
+two()
+three()
