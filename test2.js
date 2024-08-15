23221a331bd329480df6bcae711e00ae3947765d
zarif007008 <zarif007008az@gmail.com>
2024-08-15 20:56:27 +0600

    practicing with mahi

--- test2.js
+++ test2.js
@@ -1,2 +1,27 @@
 console.log("yo");
 console.log("yo2");
+
+let value1 = 30
+let value2 = 20
+
+let result = value1 + value2
+console.log(result);
+
+function mahi(){
+    console.log("mahi is a chutiya");
+}
+mahi()
+
+const mahi2 = () => {
+    console.log("mahi is a biggest chutiya");
+}
+mahi2();
+
+let mahi3 = () => console.log("big");
+mahi3()
+
+if (true){
+    console.log("");
+}
+
+(true) ? console.log("") : console.log("2");
