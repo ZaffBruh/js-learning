4524118f7e262995e598866198b61b47901e8f14
zarif007008 <zarif007008az@gmail.com>
2024-08-22 18:59:03 +0600

    first color changed

---
+++ js_project/colorChanger/index.js
@@ -0,0 +1,13 @@
+const buttons = document.querySelectorAll("button")
+const body = document.querySelector("body")
+
+buttons.forEach((button) => {
+    console.log(button);
+    button.addEventListener("click", function(e) {
+        console.log(e);
+        console.log(e.target);
+        if(e.target.id === "g"){
+            body.style.backgroundColor = "green"
+        }
+    })
+})
