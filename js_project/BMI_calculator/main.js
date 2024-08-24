02e92344f2921bd831700ee296f31f2a77643f61
zarif007008 <zarif007008az@gmail.com>
2024-08-24 20:25:53 +0600

    calculation done

--- js_project/BMI_calculator/main.js
+++ js_project/BMI_calculator/main.js
@@ -1,11 +1,31 @@
-const weight = document.querySelector(".weight");
-const height = document.querySelector(".height");
-const btn = document.querySelector(button);
-const result = document.querySelector(".output")
+const form = document.querySelector("form")
 
-btn.forEach(function (btn) {
-    btn.addEventLstener("click",function (e){
-        e = weight/(height*height)
-        body.output = e;
-    })
+// this usecase will give an empty value
+
+// const weight = document.querySelector("#weight");
+// const height = document.querySelector("#height");
+// const btn = document.querySelector(button);
+// const result = document.querySelector("#output")
+
+
+form.addEventListener("submit",function (e){
+    e.preventDefault()
+
+    const height = parseInt(document.querySelector("#height").value)
+    const weight = parseInt(document.querySelector("#weight").value)
+    const output = document.querySelector("#output")
+
+    if(height === "" || height < 0 || isNaN(height)){
+        output.innerHTML="Please give a valid height"
+    }
+    
+    else if(weight === "" || weight < 0 || isNaN(weight)){
+        output.innerHTML="Please give a valid weight"
+    }
+    
+    else {
+        const bmi = (weight/((height*height)/10000)).toFixed(2)
+        //show the output
+        output.innerHTML = bmi
+    }
 })
