f06223d6f6ad8e4af9ee477709420b3dd7010b2b
zarif007008 <zarif007008az@gmail.com>
2024-08-24 20:54:46 +0600

    output

--- js_project/BMI_calculator/main.js
+++ js_project/BMI_calculator/main.js
@@ -14,6 +14,7 @@ form.addEventListener("submit",function (e){
     const height = parseInt(document.querySelector("#height").value)
     const weight = parseInt(document.querySelector("#weight").value)
     const output = document.querySelector("#output")
+    const output2 = document.querySelector("#output2")
 
     if(height === "" || height < 0 || isNaN(height)){
         output.innerHTML="Please give a valid height"
@@ -27,5 +28,15 @@ form.addEventListener("submit",function (e){
         const bmi = (weight/((height*height)/10000)).toFixed(2)
         //show the output
         output.innerHTML = bmi
+        if(bmi<18.6){
+            output2.innerHTML = "You are under weight"
+        }
+        else if(bmi >= 18.6 && bmi <= 24.9){
+            output2.innerHTML = "You are normal. Keep it up."
+        }
+        else if(bmi > 24.9){
+            output2.innerHTML = "You are overweight"
+        }
     }
+    
 })
