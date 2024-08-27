f08568c7955e50e2d877d155577520178edc6b09
zarif007008 <zarif007008az@gmail.com>
2024-08-27 23:59:24 +0600

    successfully added

--- js_project/Guess the number/main.js
+++ js_project/Guess the number/main.js
@@ -3,11 +3,13 @@ const body = document.querySelector("body")
 
 bg.forEach((bg) => {
     bg.addEventListener("click", function(e){
-        if(e.target.id==="bttn"){
-            body.style.backgroundColor= "red";
+        if(e.target.id === "white"){
+            body.style.backgroundColor = "white";
+            body.style.color = "black"; 
         }
-        if(e.target.id==="bttn1"){
-            body.style.backgroundColor= "green";
+        if(e.target.id === "black"){
+            body.style.backgroundColor = "black";
+            body.style.color = "white";
         }
     })
 })
