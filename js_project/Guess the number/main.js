624f28f8f903711921c0b7b42ba24aea86520eef
zarif007008 <zarif007008az@gmail.com>
2024-08-28 18:47:37 +0600

    functions of guessing game

--- js_project/Guess the number/main.js
+++ js_project/Guess the number/main.js
@@ -19,3 +19,74 @@ bg.forEach((bg) => {
         }   
     })
 })
+
+// +++++++++++ guessing number script ++++++++++++++++
+
+const randomNum = parseInt(Math.random() * 100 + 1);
+
+const submit = document.querySelector("#btn")
+const userInput = document.querySelector("#input")
+const previousGuesses = document.querySelector("#prevNum")
+const remainGuess = document.querySelector("#remainGuess")
+const lowOrHigh = document.querySelector(".lowOrHigh")
+const startOver = document.querySelector(".resultPara")
+
+const p = document.createElement("p")
+
+let prevGuess = []
+let numGuess = 1
+
+let playGame = true
+
+if(playGame){
+    submit.addEventListener("click", function(e){
+        e.preventDefault()
+
+        const guess = parseInt(userInput.value)
+        validateGuess(guess)
+    })
+}
+
+function validateGuess(guess){
+    if(isNaN(guess)){
+        alert("Please enter a valid number")
+    }
+    else if(guess < 1){
+        alert("Please enter a greater than 1 number")
+    }
+    else if(guess > 100){
+        alert("Please enter a less than 100 number")
+    }
+    else{
+        prevGuess.push(guess)
+        if(numGuess === 11){
+            displayGuess(guess)
+            displayMsg(`Game Over. Random number was ${randomNum}`)
+            endGame()
+        }
+        else{
+            displayGuess(guess)
+            checkGuess(guess)
+        }
+    }
+}
+
+function checkGuess(guess){
+
+}
+
+function displayGuess(guess){
+
+}
+
+function displayMsg(msg){
+
+}
+
+function newGame(){
+
+}
+
+function endGame(){
+
+}
