188a6fff919ae461ba088621f01a55d5ed9ee381
zarif007008 <zarif007008az@gmail.com>
2024-09-09 02:11:51 +0600

    unlimitedColor project done

--- js_project/unlimitedColor/main.js
+++ js_project/unlimitedColor/main.js
@@ -8,10 +8,27 @@ start.addEventListener("click",function(){
      set = setInterval(function(){
         bgColor.style.backgroundColor = "blue"
     },3000)
+     set2 = setInterval(function(){
+        bgColor.style.backgroundColor = "red"
+    },6000)
+     set3 = setInterval(function(){
+        bgColor.style.backgroundColor = "green"
+    },9000)
+     set4 = setInterval(function(){
+        bgColor.style.backgroundColor = "purple"
+    },12000)
+     set5 = setInterval(function(){
+        bgColor.style.backgroundColor = "yellow"
+    },15000)
+    
 })
 
 stop.addEventListener("click", function(){
     console.log("stopped");
     clearInterval(set)
+    clearInterval(set2)
+    clearInterval(set3)
+    clearInterval(set4)
+    clearInterval(set5)
 })
 
