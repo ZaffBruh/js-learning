5ef6fe8cf01c4a8d08e361ca56b2e60c723f3d22
zarif007008 <zarif007008az@gmail.com>
2024-09-12 14:35:08 +0600

    just checking

---
+++ test/main.js
@@ -0,0 +1,7 @@
+let btn = document.querySelector("button")
+let head = document.querySelector("h1")
+
+btn.addEventListener("click", function(){
+    head.innerHTML = "rolek becomes a programmer"
+    head.style.color = "red"
+})
